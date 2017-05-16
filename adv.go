5c0fea70b2ec90f322fdcc02d836afbe6691f858
Hisaya Tanaka <tanaka_hisaya@cygames.co.jp>
2017-05-16 18:40:22 +0900

    Central動作時のスキャニングがエラーで落ちる件の修正

--- adv.go
+++ adv.go
@@ -96,6 +96,9 @@ func (a *Advertisement) unmarshall(b []byte) error {
 		if len(b) < int(1+l) {
 			return errors.New("invalid advertise data")
 		}
+		if l==0 {
+			return errors.New("invalid advertise data")
+		}
 		d := b[2 : 1+l]
 		switch t {
 		case typeFlags:
